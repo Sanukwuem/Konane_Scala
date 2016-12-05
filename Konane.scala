def99f53648053494a269f039496ab7dff8a62f2
Sean Anukwuem <anukwus1@tcnj.edu>
2016-12-04 21:54:40 -0500

    fixed empty

--- Konane.scala
+++ Konane.scala
@@ -7,7 +7,7 @@ var count = 0
 var numbOfColumns = 8
 
 
-val boardArray = Array.ofDim[String](9,9)
+val boardArray = Array.ofDim[String](8, 8)
 ///////////////////////////////////////////
 
 // Start of game
@@ -15,55 +15,52 @@ initialBoard
 pickPieces
 mainGame
 
-
-
-
 //////////////////////////////////////////////////////////////////////////////////////////
 
 //board
 def initialBoard = {
-var numbOfRows = 8
-print("\n   ")
-for (n <- 1 to numbOfColumns ){
-print(n + "   ")
-}
-for (row <- 0 until boardSize){
+	var numbOfRows = 8
+	print("\n   ")
+	for (n <- 1 to numbOfColumns ){
+		print(n + "   ")
+	}
+	for (row <- 0 until boardSize){
 		println()
 		print (numbOfRows + " |")
 		numbOfRows = numbOfRows - 1
-	for (col <- 0 until boardSize) {
-		count = count + 1
-		
-		
-		if (count > 0 && count <= 8
-		|| count > 16 && count <= 24
-		|| count > 32 && count <= 40
-		|| count > 48 && count <= 56) { 
-		if (count%2 == 0){
-		boardArray(row)(col) = "O"
-		print(boardArray(row)(col) + " |" + " ")
+		for (col <- 0 until boardSize) {
+			count = count + 1
+			
+			
+			if (count > 0 && count <= 8
+			|| count > 16 && count <= 24
+			|| count > 32 && count <= 40
+			|| count > 48 && count <= 56) { 
+				if (count%2 == 0){
+					boardArray(row)(col) = "O"
+					print(boardArray(row)(col) + " |" + " ")
+				}
+				else {
+					boardArray(row)(col) = "X"
+					print(boardArray(row)(col) + " |" + " ")
+				}
+			}
+			
+			if (count > 8 && count <= 16
+			|| count > 24 && count <= 32
+			|| count > 40 && count <= 48
+			|| count > 56 && count <= 64) {
+				if (count%2 == 0){
+					boardArray(row)(col) = "X"
+					print(boardArray(row)(col) + " |" + " ")
+				}
+				else {
+					boardArray(row)(col) = "O"
+					print(boardArray(row)(col) + " |" + " ")
+				}
+			}	
 		}
-		else {
-		boardArray(row)(col) = "X"
-		print(boardArray(row)(col) + " |" + " ")
-		}
-		}
-		
-		if (count > 8 && count <= 16
-		|| count > 24 && count <= 32
-		|| count > 40 && count <= 48
-		|| count > 56 && count <= 64) {
-		if (count%2 == 0){
-		boardArray(row)(col) = "X"
-		print(boardArray(row)(col) + " |" + " ")
-		}
-		else {
-		boardArray(row)(col) = "O"
-		print(boardArray(row)(col) + " |" + " ")
-		}
-		}	
 	}
-}
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////
@@ -80,30 +77,30 @@ def pickPieces = {
 	printBoard
 	
 	if(row == 1 && col == 8){
-	println("\n\nPick Adjacent Piece. Choices include (1,7) and (2,8)")
-	println("\nEnter Row")
-	val row = scala.io.StdIn.readInt()
-	println("Enter Column")
-	val col = scala.io.StdIn.readInt()
-	empty(row,col)	
+		println("\n\nPick Adjacent Piece. Choices include (1,7) and (2,8)")
+		println("\nEnter Row")
+		val row = scala.io.StdIn.readInt()
+		println("Enter Column")
+		val col = scala.io.StdIn.readInt()
+		empty(row,col)	
 	}
 	
 	if(row == 4 && col == 5){
-	println("\n\nPick Adjacent Piece. Choices include (3,5) (4,6) (5,5) and (4,4)")
-	println("\nEnter Row")
-	val row = scala.io.StdIn.readInt()
-	println("Enter Column")
-	val col = scala.io.StdIn.readInt()
-	empty(row,col)
+		println("\n\nPick Adjacent Piece. Choices include (3,5) (4,6) (5,5) and (4,4)")
+		println("\nEnter Row")
+		val row = scala.io.StdIn.readInt()
+		println("Enter Column")
+		val col = scala.io.StdIn.readInt()
+		empty(row,col)
 	}
 	
 	if(row == 8 && col == 1){
-	println("\n\nPick Adjacent Piece. Choices include (7,1) and (8,2)")
-	println("\nEnter Row")
-	val row = scala.io.StdIn.readInt()
-	println("Enter Column")
-	val col = scala.io.StdIn.readInt()
-	empty(row,col)
+		println("\n\nPick Adjacent Piece. Choices include (7,1) and (8,2)")
+		println("\nEnter Row")
+		val row = scala.io.StdIn.readInt()
+		println("Enter Column")
+		val col = scala.io.StdIn.readInt()
+		empty(row,col)
 	}
 	printBoard	
 }
@@ -161,32 +158,17 @@ def mainGame: String = {
 	println("Light Pieces Left " + countO)
 	
 	if(countX == 0){
-	println("\n\nGame over, Light Player Wins")
-	System.exit(0)
+		println("\n\nGame over, Light Player Wins")
+		System.exit(0)
 	}
 	if(countO == 0){
-	println("\n\nGame over, Light Player Wins")
-	System.exit(0)
+		println("\n\nGame over, Light Player Wins")
+		System.exit(0)
 	}
 	
 	mainGame
 	
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 def makeJump(row:Int, col:Int, row2:Int, col2:Int) = {
 	jump(row,col,row2,col2)
@@ -228,15 +210,15 @@ def makeJump(row:Int, col:Int, row2:Int, col2:Int) = {
 }
 
 def printBoard = {
-var numbOfRows = 8
+	var numbOfRows = 8
 	
 	// 1 to 8
 	print("   ")
 	for (n <- 1 to numbOfColumns ){
-	print(n + "   ")
+		print(n + "   ")
 	}
 	for (col <- 0 until boardSize) {
-	// 8 to 1
+		// 8 to 1
 		println()
 		print (numbOfRows + " |")
 		numbOfRows = numbOfRows - 1
@@ -247,133 +229,27 @@ var numbOfRows = 8
 }
 
 def empty(row:Int, col:Int) = {
-	if(col == 8){
-	boardArray(row-1)(col-8) = "."	
-	}
-	if(col == 7){
-	boardArray(row-1)(col-6) = "."
-	}
-	if(col == 6){
-	boardArray(row-1)(col-4) = "."
-	}
-	if(col == 5){
-	boardArray(row-1)(col-2) = "."
-	}
-	if(col == 4){
-	boardArray(row-1)(col) = "."
-	}
-	if(col == 3){
-	boardArray(row-1)(col+2) = "."
-	}
-	if(col == 2){
-	boardArray(row-1)(col+4) = "."
-	}
-	if(col == 1){
-	boardArray(row-1)(col+6) = "."
-	}
+
+	val newCol = colConversion(col)
+	boardArray(row-1)(newCol) = "."
+	
 }
 
 def jump(row:Int, col:Int, row2:Int, col2:Int) = {
-	
-	if(col2 == 8){
-	//vertical
-	if(col == 6){
-	boardArray(row2-1)(col2-8) = boardArray(row-1)(col-4)
-	}
-	if(col == 4){
-	boardArray(row2-1)(col2-8) = boardArray(row-1)(col)
-	}
-	else{
-	boardArray(row2-1)(col2-8) = boardArray(row-1)(col-8)
-	}	
-	}
-	if(col2 == 7){
-	if(col == 5){
-	boardArray(row2-1)(col2-6) = boardArray(row-1)(col-2)
-	}
-	if(col == 3){
-	boardArray(row2-1)(col2-6) = boardArray(row-1)(col+2)
-	}
-	else{
-	boardArray(row2-1)(col2-6) = boardArray(row-1)(col-6)
-	}
-	}
-	if(col2 == 6){
-	if(col == 8){
-	boardArray(row2-1)(col2-4) = boardArray(row-1)(col-8)
-	}
-	if(col == 4){
-	boardArray(row2-1)(col2-4) = boardArray(row-1)(col)
-	}
-	if(col == 2){
-	boardArray(row2-1)(col2-4) = boardArray(row-1)(col+4)
-	}
-	else{
-	boardArray(row2-1)(col2-4) = boardArray(row-1)(col-4)
-	}
-	}
-	if(col2 == 5){
-	if(col == 7){
-	boardArray(row2-1)(col2-2) = boardArray(row-1)(col-6)
-	}
-	if(col == 3){
-	boardArray(row2-1)(col2-2) = boardArray(row-1)(col+2)
-	}
-	if(col == 1){
-	boardArray(row2-1)(col2-2) = boardArray(row-1)(col+6)
-	}
-	else{
-	boardArray(row2-1)(col2-2) = boardArray(row-1)(col-2)
-	}
-	}
-	if(col2 == 4){
-	if(col == 8){
-	boardArray(row2-1)(col2) = boardArray(row-1)(col-8)
-	}
-	if(col == 6){
-	boardArray(row2-1)(col2) = boardArray(row-1)(col-4)
-	}
-	if(col == 2){
-	boardArray(row2-1)(col2) = boardArray(row-1)(col+4)
-	}
-	else{
-	boardArray(row2-1)(col2) = boardArray(row-1)(col)
-	}
-	}
-	if(col2 == 3){
-	if(col == 7){
-	boardArray(row2-1)(col2+2) = boardArray(row-1)(col-6)
-	}
-	if(col == 5){
-	boardArray(row2-1)(col2+2) = boardArray(row-1)(col-2)
-	}
-	if(col == 1){
-	boardArray(row2-1)(col2+2) = boardArray(row-1)(col+6)
-	}
-	else{
-	boardArray(row2-1)(col2+2) = boardArray(row-1)(col+2)
-	}
-	}
-	if(col2 == 2){
-	if(col == 4){
-	boardArray(row2-1)(col2+4) = boardArray(row-1)(col)
-	}
-	if(col == 6){
-	boardArray(row2-1)(col2+4) = boardArray(row-1)(col-4)
-	}
-	else{
-	boardArray(row2-1)(col2+4) = boardArray(row-1)(col+4)
-	}
-	}
-	if(col2 == 1){
-	if(col == 3){
-	boardArray(row2-1)(col2+6) = boardArray(row-1)(col+2)
-	}
-	if(col == 5){
-	boardArray(row2-1)(col2+6) = boardArray(row-1)(col-2)
-	}
-	else{
-	boardArray(row2-1)(col2+6) = boardArray(row-1)(col+6)
-	}
-	}
+
+	val newCol = colConversion(col)
+	val newCol2 = colConversion(col2)
+
+	boardArray(row2-1)(newCol2) = boardArray(row-1)(newCol)
+}
+
+def colConversion(col: Int): Int = col match {
+	case 8 => 0
+	case 7 => 1
+	case 6 => 2
+	case 5 => 3
+	case 4 => 4
+ 	case 3 => 5
+ 	case 2 => 6
+ 	case 1 => 7
 }
